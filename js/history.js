b6bdc3e75f34c7310e0d039509f0d3c9030317b5
Naz17-cloud <mnasrul220@gmail.com>
2026-06-06 19:22:36 +0700

    Update history.js

--- js/history.js
+++ js/history.js
@@ -1,14 +1,62 @@
-// ==========================================
-// HISTORY PAGE
-// ==========================================
-
 document.addEventListener(
-    "DOMContentLoaded",
-    () => {
-
-        console.log(
-            "History Ready"
-        );
-
-    }
+"DOMContentLoaded",
+loadHistory
 );
+
+function loadHistory(){
+
+const data =
+JSON.parse(
+localStorage.getItem(
+"letters"
+)
+) || [];
+
+const tbody =
+document.getElementById(
+"historyTable"
+);
+
+tbody.innerHTML = "";
+
+data.forEach(letter => {
+
+tbody.innerHTML += `
+
+<tr>
+
+<td>${letter.number}</td>
+
+<td>${letter.requester}</td>
+
+<td>${letter.email}</td>
+
+<td>
+${letter.number.split('/')[1]}
+</td>
+
+<td>
+${letter.subject}
+</td>
+
+<td>
+${letter.createdAt}
+</td>
+
+<td>
+
+<span class="status-active">
+
+${letter.status}
+
+</span>
+
+</td>
+
+</tr>
+
+`;
+
+});
+
+}
