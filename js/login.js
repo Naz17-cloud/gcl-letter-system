223ac214c4f0d3f94e1a957f904326d3cd4d2ac3
Naz17-cloud <mnasrul220@gmail.com>
2026-06-06 22:10:58 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,11 +1,95 @@
-if(role === "admin"){
+document.addEventListener(
+"DOMContentLoaded",
+() => {
 
-    window.location.href =
-    "admin.html";
+```
+    const loginForm =
+        document.getElementById(
+            "loginForm"
+        );
 
-}else{
+    loginForm.addEventListener(
+        "submit",
+        function(e){
 
-    window.location.href =
-    "request.html";
+            e.preventDefault();
+
+            const email =
+                document
+                .getElementById(
+                    "email"
+                )
+                .value
+                .trim()
+                .toLowerCase();
+
+            const password =
+                document
+                .getElementById(
+                    "password"
+                )
+                .value
+                .trim();
+
+            if(
+                !email ||
+                !password
+            ){
+
+                alert(
+                    "Email dan Password wajib diisi."
+                );
+
+                return;
+            }
+
+            // ==================================
+            // LOGIN DUMMY
+            // NANTI DIGANTI FIREBASE
+            // ==================================
+
+            let role =
+                "staff";
+
+            if(
+                email.includes(
+                    "admin"
+                )
+            ){
+
+                role =
+                "admin";
+
+            }
+
+            localStorage.setItem(
+                "currentUser",
+                email
+            );
+
+            localStorage.setItem(
+                "currentRole",
+                role
+            );
+
+            if(
+                role === "admin"
+            ){
+
+                window.location.href =
+                    "admin.html";
+
+            }else{
+
+                window.location.href =
+                    "request.html";
+
+            }
+
+        }
+    );
 
 }
+```
+
+);
