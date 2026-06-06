80c4668786d598d285ecea3f8f099b6ad589d56a
Naz17-cloud <mnasrul220@gmail.com>
2026-06-06 18:33:49 +0700

    Create admin.js

---
+++ js/admin.js
@@ -0,0 +1,15 @@
+// =======================================
+// GCL LETTER SYSTEM
+// ADMIN SCRIPT v1.0
+// =======================================
+
+document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+
+        console.log(
+            "Admin Dashboard Ready"
+        );
+
+    }
+);
