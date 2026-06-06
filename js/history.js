5b5e5bcba5297e48978a3d0cfbca917a09899cda
Naz17-cloud <mnasrul220@gmail.com>
2026-06-06 18:30:22 +0700

    Create history.js

---
+++ js/history.js
@@ -0,0 +1,14 @@
+// ==========================================
+// HISTORY PAGE
+// ==========================================
+
+document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+
+        console.log(
+            "History Ready"
+        );
+
+    }
+);
